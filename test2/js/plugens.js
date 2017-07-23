0af7c8b8e67b594ec8bf98edcbefdafc738bb570
H-Elsharkawy75 <eh100@yahoo.com>
2017-07-24 00:43:41 +0200

    test2

---
+++ test2/js/plugens.js
@@ -0,0 +1,18 @@
+$(document).ready(function()
+   { 
+    
+    $(".navbar .fa").click(function()
+        {
+            $(".navbar .form-control").toggle();
+        
+        });
+    
+   //  $("a").click(function()
+    // {
+        
+        //   $(this).css("color","#0B5D8E");
+           
+          
+    // });
+        
+   });
